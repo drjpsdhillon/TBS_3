43b973646b7b81b0130f95c77fbf23aeaef3cdbf
drjpsdhillon <drjpsdhillon@gmail.com>
2026-09-10 08:27:55 +0530

    refactor: inline tab management logic into index.html and remove redundant build/extraction scripts

--- check_js.py
+++
@@ -1,9 +0,0 @@
-import re
-with open("index.html", "r", encoding="utf-8") as f:
-    html = f.read()
-
-m = re.search(r"<script>([\s\S]*?)</script>", html)
-if m:
-    with open("temp_script.js", "w", encoding="utf-8") as f_out:
-        f_out.write(m.group(1))
-    print("Wrote temp_script.js, length:", len(m.group(1)))
--- parse_test.py
+++
@@ -1,8 +0,0 @@
-import esprima
-try:
-    with open("temp_script.js", "r", encoding="utf-8") as f:
-        code = f.read()
-    esprima.parseScript(code)
-    print("VALID JAVASCRIPT SYNTAX!")
-except Exception as e:
-    print("PARSE ERROR:", e)
